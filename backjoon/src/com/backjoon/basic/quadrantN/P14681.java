be46f4048c7d157fa6d528cb99d60c69618fb666
jyebe9034 <jyebe9034@gmail.com>
2021-07-15 21:17:55 +0900

    second commit

---
+++ backjoon/src/com/backjoon/basic/quadrantN/P14681.java
@@ -0,0 +1,20 @@
+package com.backjoon.basic.quadrantN;
+
+import java.util.Scanner;
+
+public class P14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else if (x > 0 && y < 0) {
+            System.out.println(4);
+        }
+    }
+}
