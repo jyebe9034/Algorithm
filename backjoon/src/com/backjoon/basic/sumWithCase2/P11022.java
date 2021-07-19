7a5b67d76ad3b6e28ed43dca26a21d1316c4c7fb
jyebe9034 <jyebe9034@gmail.com>
2021-07-19 20:19:19 +0900

    fourth commit

---
+++ backjoon/src/com/backjoon/basic/sumWithCase2/P11022.java
@@ -0,0 +1,21 @@
+package com.backjoon.basic.sumWithCase2;
+
+import java.util.Scanner;
+
+public class P11022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+        String[] inputs = new String[T];
+        for (int i = 0; i < inputs.length; i++) {
+            inputs[i] = sc.nextLine();
+        }
+        for (int i = 0; i < inputs.length; i++) {
+            String input = inputs[i];
+            String[] arr = input.split(" ");
+            int A = Integer.parseInt(arr[0]);
+            int B = Integer.parseInt(arr[1]);
+            System.out.println("Case #" + (i+1) + ": " + A + " + " + B + " = "  + (A+B));
+        }
+    }
+}
