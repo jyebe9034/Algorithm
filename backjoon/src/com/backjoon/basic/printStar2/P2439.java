68f245905b118e8d02ed74106db49e316053811b
jyebe9034 <jyebe9034@gmail.com>
2021-07-20 20:49:04 +0900

    fifth commit

---
+++ backjoon/src/com/backjoon/basic/printStar2/P2439.java
@@ -0,0 +1,18 @@
+package com.backjoon.basic.printStar2;
+
+import java.util.Scanner;
+
+public class P2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        String star = "";
+        for (int i = 0; i < input; i++) {
+            for (int j = 1; j < input - i; j++) {
+                System.out.print(" ");
+            }
+            star += "*";
+            System.out.println(star);
+        }
+    }
+}
