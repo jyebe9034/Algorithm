bef0f1e5afca2d248281d70a53b591210b2acc3e
jyebe9034 <jyebe9034@gmail.com>
2021-07-27 23:04:20 +0900

    eighth commit

---
+++ backjoon/src/com/backjoon/basic/oneDemensionalArray/average/P1546.java
@@ -0,0 +1,22 @@
+package com.backjoon.basic.oneDemensionalArray.average;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = Integer.parseInt(sc.nextLine());
+        double[] numbers = new double[count];
+        for (int i = 0; i < count; i++) {
+            numbers[i] = Integer.parseInt(sc.nextLine());
+        }
+        double max = Arrays.stream(numbers).max().getAsDouble();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i] / max * 100;
+        }
+        double newAverage = Arrays.stream(numbers).average().getAsDouble();
+        System.out.println(newAverage);
+    }
+}
+
--- backjoon/src/com/backjoon/basic/oneDemensionalArray/remainder/P3052.java
+++ backjoon/src/com/backjoon/basic/oneDemensionalArray/remainder/P3052.java
@@ -1,7 +1,21 @@
 package com.backjoon.basic.oneDemensionalArray.remainder;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class P3052 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int number = 42;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int input = Integer.parseInt(sc.nextLine());
+            int remainder = input % number;
+            boolean isTheSame = list.stream().anyMatch(one -> one == remainder);
+            if (!isTheSame) {
+                list.add(remainder);
+            }
+        }
+        System.out.println(list.size());
     }
 }
