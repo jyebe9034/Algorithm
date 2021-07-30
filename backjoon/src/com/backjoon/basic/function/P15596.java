920b21ab0f7d43e291e0175c671ad70a212c0c40
jyebe9034 <jyebe9034@gmail.com>
2021-07-30 21:22:02 +0900

    eleventh commit

---
+++ backjoon/src/com/backjoon/basic/function/P15596.java
@@ -0,0 +1,11 @@
+package com.backjoon.basic.function;
+
+public class P15596 {
+    public long sum(int[] a) {
+        long sum = 0L;
+        for (int num : a) {
+            sum += num;
+        }
+        return sum;
+    }
+}
